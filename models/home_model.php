faee522d54a55d2fe237f41f0eab057395639f2c
dev-ubuntu <amguecha17021@gmail.com>
2021-11-30 16:48:15 +0000

    Update to V.0.2

---
+++ models/home_model.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * SAMPLE CONTENT. Edit or delete it to start a clean project.
+ * 
+ * @method: data()      -> BASIC MODEL METHOD that 'manipulates' some  
+ *                         strings and returns them to the controller.
+ * @method: // create() -> Method (disabled) to INSERT data in a sample  
+ *                         table called 'crud_table'.
+ * @method: // read()   -> Method (disabled) to SELECT data in a sample  
+ *                         table called 'crud_table'.
+ * @method: // update() -> Method (disabled) to UPDATE data in a sample  
+ *                         table called 'crud_table'.
+ * @method: // delete() -> Method (disabled) to DELETE data in a sample  
+ *                         table called 'crud_table'.
+ * 
+ */
+class home_model
+{
+    public function data()
+    {
+        $string_one = 'Simple ';
+        $string_two = 'MVC Front Controller ';
+        $string_three = 'Mini Framework ';
+        $string_four = '(V.0.2)';
+        $secondary_heading = $string_one . $string_two . $string_three . $string_four;
+        return $secondary_heading;
+    }
+
+    // public function create($title, $description)
+    // {
+    //     $conn = new database();
+    //     $sql = "INSERT INTO crud_table VALUES( NULL, :title, :description )";
+    //     $conn->query($sql);
+    //     $conn->bind(':title', $title);
+    //     $conn->bind(':description', $description);
+    //     $conn->execute();
+    // }
+
+    // public function read()
+    // {
+    //     $conn = new database();
+    //     $sql = "SELECT * FROM crud_table";
+    //     $conn->query($sql);
+    //     $rows = $conn->fetch_all();
+    //     return $rows;
+    // }
+
+    // public function update($id, $title, $description)
+    // {
+    //     $conn = new database();
+    //     $sql = "UPDATE crud_table SET title = :title, description = :description WHERE id = :id";
+    //     $conn->query($sql);
+    //     $conn->bind(':id', $id);
+    //     $conn->bind(':title', $title);
+    //     $conn->bind(':description', $description);
+    //     $conn->execute();
+    // }
+
+    // public function delete($id)
+    // {
+    //     $conn = new database();
+    //     $sql = "DELETE FROM crud_table WHERE id = :id";
+    //     $conn->query($sql);
+    //     $conn->bind(':id', $id);
+    //     $conn->execute();
+    // }
+}
+
