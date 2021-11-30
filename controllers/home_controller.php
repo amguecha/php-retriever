1435f6195abdb6fc9750fdd7cfb6056630a4081d
amguecha <amaro17021@gmail.com>
2021-11-30 20:23:48 +0000

    Update home_controller.php

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -10,15 +10,20 @@ class home_controller
 	public function index()
 	{
 		/** SAMPLE CONTENT. Edit or delete it to start a clean project. */
-
-		$view = new retriever('view');
-		$view->set_file('index.php');
+		
+		$var_a = 'Simple ';
+		$var_b = 'MVC Front Controller ';
 
 		$model = new retriever('model');
 		$model->set_file('home_model.php');
+		$model->set_data([
+			'a' => $var_a
+		]);
 
+		$view = new retriever('view');
+		$view->set_file('index.php');
 		$view->set_data([
-			'secondary_heading' => $model->call()->data()
+			'secondary_heading' => $model->call()->data($var_b)
 		]);
 
 		$view->render();
