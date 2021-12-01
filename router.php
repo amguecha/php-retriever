a58ee643356df815ffa6cbeaccbeb3b81227cdad
amguecha <amaro17021@gmail.com>
2021-12-01 13:50:44 +0000

    Update router.php

--- router.php
+++ router.php
@@ -19,14 +19,14 @@ class router
 		header('x-frame-options:' . configuration::XFO);
 
 		/** Getting number of section delimiters '/' in URL */
-		$uriSections = substr_count($_SERVER['REQUEST_URI'], '/');
+		$uri_s = substr_count($_SERVER['REQUEST_URI'], '/');
 
 		/** 
 		 * CHECKING and FILTERING the URL structure (www.domain.com/section/section/). 
 		 * More section delimiters are not supported by default on this framework.
 		 * 
 		 */
-		if ($uriSections <= 3) {
+		if ($uri_s <= 3) {
 
 			/** 
 			 * INCLUDING URL variable names and values as sections,
@@ -38,76 +38,89 @@ class router
 			$request = str_replace($replace, '/', $_SERVER['REQUEST_URI']);
 			$uri = explode('/', $request);
 
-			/** www.domain.com/a/b/ */
-			if ($uriSections == 3 && !empty($uri[1]) && !empty($uri[2]) && empty($uri[3])) {
-				$controllerName = $uri[1] . '_controller';
-				$actionName = $uri[2];
+			/** Setting up variables with the default controller and action names */
+			$defautl_c = configuration::DEFAULT_CONTROLLER;
+			$defautl_a = configuration::DEFAULT_ACTION;
+
+			/** www.domain.com/a/b/ but, NOT pointing to defaults */
+			if ($uri_s == 3 && !empty($uri[1]) && !empty($uri[2]) && empty($uri[3])) {
+				$controller = $uri[1] . '_controller';
+				$action = $uri[2];
+				if ($controller === $defautl_c && $action === $defautl_a) {
+					goto Not_Found;
+				}
 			}
-			/** www.domain.com/a/b */
-			elseif ($uriSections == 2 && !empty($uri[1]) && !empty($uri[2])) {
-				$controllerName = $uri[1] . '_controller';
-				$actionName = $uri[2];
+			/** www.domain.com/a/b but, NOT pointing to defaults */
+			elseif ($uri_s == 2 && !empty($uri[1]) && !empty($uri[2])) {
+				$controller = $uri[1] . '_controller';
+				$action = $uri[2];
+				if ($controller === $defautl_c && $action === $defautl_a) {
+					goto Not_Found;
+				}
 			}
-			/** www.domain.com/a/ */
-			elseif ($uriSections == 2 && !empty($uri[1]) && empty($uri[2])) {
-				$controllerName = configuration::DEFAULT_CONTROLLER;
-				$actionName = $uri[1];
+			/** www.domain.com/a/ but, NOT pointing to defaults */
+			elseif ($uri_s == 2 && !empty($uri[1]) && empty($uri[2])) {
+				$controller = configuration::DEFAULT_CONTROLLER;
+				$action = $uri[1];
+				if ($action === $defautl_a) {
+					goto Not_Found;
+				}
 			}
-			/** www.domain.com/a */
-			elseif ($uriSections == 1 && !empty($uri[1])) {
-				$controllerName = configuration::DEFAULT_CONTROLLER;
-				$actionName = $uri[1];
+			/** www.domain.com/a but, NOT pointing to defaults */
+			elseif ($uri_s == 1 && !empty($uri[1])) {
+				$controller = configuration::DEFAULT_CONTROLLER;
+				$action = $uri[1];
+				if ($action === $defautl_a) {
+					goto Not_Found;
+				}
 			}
 			/** (DEFAULT) www.domain.com/ */
-			elseif ($uriSections == 1 && empty($uri[1])) {
-				$controllerName = configuration::DEFAULT_CONTROLLER;
-				$actionName = configuration::DEFAULT_ACTION;
+			elseif ($uri_s == 1 && empty($uri[1])) {
+				$controller = configuration::DEFAULT_CONTROLLER;
+				$action = configuration::DEFAULT_ACTION;
 			}
-			/** URL falls out of previous filters. */
+			/** NOT SUPPORTED. URL falls out of previous filters. */
 			else {
-				http_response_code(404);
-				include_once(VIEWS . 'errors/404.php');
-				trigger_error('Page not found. Arguments in URI do not match any class or method');
-				exit();
+				goto Not_Found;
 			}
 
-		/** NOT SUPPORTED. URL with more than 3 section delimiters. */
+			/** NOT SUPPORTED. URL with more than 3 section delimiters. */
 		} else {
-			http_response_code(404);
-			include_once(VIEWS . 'errors/404.php');
-			trigger_error('Not supported address. Too many slash delimiters found in URI');
-			exit();
+			goto Not_Found;
 		}
 
-		/** CONTROLLER FILE path. */
-		$controllerPath = CONTROLLERS . $controllerName . '.php';
+		/** Controller file PATH. */
+		$controller_path = CONTROLLERS . $controller . '.php';
 
-		/** Checking if the CONTROLLER FILE exists, triggering error if not. */
-		if (!is_file($controllerPath)) {
-			http_response_code(404);
-			include_once(VIEWS . 'errors/404.php');
-			trigger_error('Page not found. There is no such controller file: ' . $controllerPath);
-			exit();
+		/** Checking if CONTROLLER FILE exists. */
+		if (!is_file($controller_path)) {
+			goto Not_Found;
 		}
 
-		/** Checking if the REQUESTED METHOD exists in the controller class, triggering error if not. */
-		if (!method_exists($controllerName, $actionName)) {
+		/** 
+		 * Checking if REQUESTED METHOD exists. Default error handler. 
+		 * NOTE: Error pages are included DIRECTLY from this router, 
+		 * no controllers or models are involved in here.
+		 * 
+		 */
+		if (!method_exists($controller, $action)) {
+			Not_Found:
 			http_response_code(404);
 			include_once(VIEWS . 'errors/404.php');
-			trigger_error('Page not found. This method (' . $actionName . ') is not present in ' . $controllerPath);
+			trigger_error('Page not found. Arguments in URI do not match any route');
 			exit();
 		}
 
 		/** 
 		 * Note: Using the function 'method_exists' in the previous check  
-		 * WILL INCLUDE the file automatically, this is a 
-		 * fallback call, just in case. 
+		 * WILL INCLUDE the file automatically, this is a fallback call just 
+		 * to visualize all the process flow. 
 		 * 
 		 */
-		include_once($controllerPath);
+		include_once($controller_path);
 
 		/** CALLING the CLASS and METHOD. */
-		$controller = new $controllerName();
-		$controller->$actionName();
+		$controller = new $controller();
+		$controller->$action();
 	}
 }
